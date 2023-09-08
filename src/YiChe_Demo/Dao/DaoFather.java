4855d1364e6e19cdff3558d74a05914c92f52c23
Mr-Glacier <K15100415112@163.com>
2023-09-08 23:25:26 +0800

    Initial commit

---
+++ src/YiChe_Demo/Dao/DaoFather.java
@@ -0,0 +1,12 @@
+package YiChe_Demo.Dao;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DaoFather {
+    //父类dao
+    private Connection  connection= null;
+    private Statement statement = null;
+    //dao层所需参数
+
+}
---
+++ src/YiChe_Demo/Main.java
@@ -0,0 +1,8 @@
+package YiChe_Demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
